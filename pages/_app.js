5e0b531758ecee86ba9a15eb90806d28f593efc1
daylen <daylennguyen@gmail.com>
2021-06-06 01:51:52 -0700

    adding husky

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import PropTypes from "prop-types";
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any
 }
 
 export default MyApp
