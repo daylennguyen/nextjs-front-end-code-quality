906d87d19c20b143eb8085e61f128f1c2f9d9973
daylen <daylennguyen@gmail.com>
2021-06-06 02:01:14 -0700

    fix: borkd test

--- test/index.test.js
+++ test/index.test.js
@@ -6,9 +6,9 @@ import expect from 'expect.js'
 import App from '../pages/index.js'
 
 describe('With Enzyme', () => {
-  it('App shows "Hello world!"', () => {
+  it('Renders with 2 divs"', () => {
     const app = shallow(<App />)
 
-    expect(app.find('div').text()).to.equal('<Homee />')
+    expect(app.find('div').length).to.equal(2)
   })
 })
